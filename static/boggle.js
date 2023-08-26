f108621ef77f1ff5e02929a0b9f8c96e0ce6e0c4
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-08-25 17:11:10 -0700

    started on front-end side

--- static/boggle.js
+++ static/boggle.js
@@ -26,9 +26,43 @@ async function start() {
 /** Display board */
 
 function displayBoard(board) {
-  // $table.empty();
+  $table.empty();
+
   // loop over board and create the DOM tr/td structure
+  const $body = $("<tbody>");
+  $table.append($body);
+
+  for (let i = 0; i < 5; i++) {
+    const $row = $("<tr>");
+
+    for (let j = 0; j < 5; j++) {
+      const $cell = $("<td>")
+        .text(board[i][j])
+        .attr("id", `${i}-${j}`);
+
+      $row.append($cell);
+    }
+
+    $table.append($row);
+  }
+
 }
 
+/** Submit word to API */
+async function submitWord(evt) {
+  evt.preventDefault();
+
+  const response = await fetch('/api/score-word', {
+    method: "POST",
+    body: JSON.stringify({ "gameId": `${gameId}`, "word": $(evt.target).val() }),
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+
+  const result = await response.json();
+}
+
+$form.on("submit", ".word-input-btn", submitWord);
 
 start();
